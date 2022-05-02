0a00dbe94d659f52f57ca423a22ea0e00bb60ba9
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-05-02 16:09:15 +0200

    remove un-needed function

--- Code for ESP32/PlatformIO Project/ESP32GreenHouseTowerDIY/lib/GreenHouseTowerDIY/src/network.cpp
+++ Code for ESP32/PlatformIO Project/ESP32GreenHouseTowerDIY/lib/GreenHouseTowerDIY/src/network.cpp
@@ -71,18 +71,6 @@ Network::~Network()
     log_i("[INFO]: Destroying network object\n");
 }
 
-String humanReadableSize(const size_t bytes)
-{
-    if (bytes < 1024)
-        return String(bytes) + " B";
-    else if (bytes < (1024 * 1024))
-        return String(bytes / 1024.0) + " KB";
-    else if (bytes < (1024 * 1024 * 1024))
-        return String(bytes / 1024.0 / 1024.0) + " MB";
-    else
-        return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
-}
-
 void notFound(AsyncWebServerRequest *request)
 {
     log_i("NOT_FOUND: ");
