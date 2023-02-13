2bdc1b8bc0cd16e176e3e6bd1cf0368c96c52355
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-02-13 19:56:11 +0000

    fix: continue refactoring code-base

--- Code/PlatformIO Project/lib/GreenHouseTowerDIY/src/local/io/sensors/pH/pHsensor.cpp
+++ Code/PlatformIO Project/lib/GreenHouseTowerDIY/src/local/io/sensors/pH/pHsensor.cpp
@@ -1,149 +1,137 @@
 #include "pHsensor.hpp"
 
-Gravity_pH pH = Gravity_pH(PH_SENSOR_PIN);
-
-PHSENSOR::PHSENSOR() : _pHTopic(PH_TOPIC),
-                       _pHOutTopic(PH_OUT_TOPIC),
-                       _inputstring_array{0},
-                       _phDnPIN(PH_DN_PIN),
-                       _phUpPIN(PH_UP_PIN),
-                       _doseTimeSm(DOSE_TIME_SM),
-                       _doseTimeMed(DOSE_TIME_MED),
-                       _doseTimeLg(DOSE_TIME_LG),
-                       _inputstring(""),
-                       _input_string_complete(false)
-{
-    pinMode(_phUpPIN, OUTPUT);
-    pinMode(_phDnPIN, OUTPUT);
-    // pH probe calibration serial commands
-    Serial.println(F("Use commands \"CAL,7\", \"CAL,4\", and \"CAL,10\" to calibrate the circuit to those respective values"));
-    Serial.println(F("Use command \"CAL,CLEAR\" to clear the calibration"));
-    if (pH.begin())
-    {
-        log_i("Loaded EEPROM");
-    }
+PHSENSOR::PHSENSOR()
+    : pH(PH_SENSOR_PIN),
+      _pHTopic(PH_TOPIC),
+      _pHOutTopic(PH_OUT_TOPIC),
+      _phDnPIN(PH_DN_PIN),
+      _phUpPIN(PH_UP_PIN),
+      _doseTimeSm(DOSE_TIME_SM),
+      _doseTimeMed(DOSE_TIME_MED),
+      _doseTimeLg(DOSE_TIME_LG),
+      _inputstring(std::string()),
+      _input_string_complete(false) {
+  pinMode(_phUpPIN, OUTPUT);
+  pinMode(_phDnPIN, OUTPUT);
 }
 
-PHSENSOR::~PHSENSOR()
-{
+PHSENSOR::~PHSENSOR() {}
+
+void PHSENSOR::begin() {
+  // pH probe calibration serial commands
+  Serial.println(
+      F("Use commands \"CAL,7\", \"CAL,4\", and \"CAL,10\" to calibrate the "
+        "circuit to those respective values"));
+  Serial.println(F("Use command \"CAL,CLEAR\" to clear the calibration"));
+  if (pH.begin()) {
+    log_i("Loaded EEPROM");
+  }
 }
 
-void PHSENSOR::serialEvent()
-{                                              // if the hardware serial port_0 receives a char
-    _inputstring = Serial.readStringUntil(13); // read the string until we see a <CR>
-    _input_string_complete = true;             // set the flag used to tell if we have received a completed string from the PC
+void PHSENSOR::serialEvent() {  // if the hardware serial port_0 receives a char
+  _inputstring.assign(Serial.readStringUntil(13)
+                          .c_str());  // read the string until we see a <CR>
+  _input_string_complete = true;      // set the flag used to tell if we have
+                                      // received a completed string from the PC
 }
 
-void PHSENSOR::parse_cmd(char *string)
-{
-    strupr(string);
-    if (strcmp(string, "CAL,7") == 0)
-    {
-        pH.cal_mid();
-        log_i("MID CALIBRATED");
-    }
-    else if (strcmp(string, "CAL,4") == 0)
-    {
-        pH.cal_low();
-        log_i("LOW CALIBRATED");
-    }
-    else if (strcmp(string, "CAL,10") == 0)
-    {
-        pH.cal_high();
-        log_i("HIGH CALIBRATED");
-    }
-    else if (strcmp(string, "CAL,CLEAR") == 0)
-    {
-        pH.cal_clear();
-        log_i("CALIBRATION CLEARED");
-    }
-    else if (strcmp(string, "PHUP") == 0)
-    {
-        digitalWrite(_phUpPIN, HIGH);
-        my_delay(_doseTimeSm);
-        digitalWrite(_phUpPIN, LOW); // Dose of pH up
-    }
-    else if (strcmp(string, "PHDN") == 0)
-    {
-        digitalWrite(_phDnPIN, HIGH);
-        my_delay(_doseTimeSm);
-        digitalWrite(_phDnPIN, LOW); // Dose of pH down
-    }
+void PHSENSOR::parse_cmd(const char* string) {
+  strupr((char*)string);
+  if (strcmp(string, "CAL,7") == 0) {
+    pH.cal_mid();
+    log_i("MID CALIBRATED");
+  } else if (strcmp(string, "CAL,4") == 0) {
+    pH.cal_low();
+    log_i("LOW CALIBRATED");
+  } else if (strcmp(string, "CAL,10") == 0) {
+    pH.cal_high();
+    log_i("HIGH CALIBRATED");
+  } else if (strcmp(string, "CAL,CLEAR") == 0) {
+    pH.cal_clear();
+    log_i("CALIBRATION CLEARED");
+  } else if (strcmp(string, "PHUP") == 0) {
+    digitalWrite(_phUpPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeSm);
+    digitalWrite(_phUpPIN, LOW);  // Dose of pH up
+  } else if (strcmp(string, "PHDN") == 0) {
+    digitalWrite(_phDnPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeSm);
+    digitalWrite(_phDnPIN, LOW);  // Dose of pH down
+  }
 }
 
-void PHSENSOR::getPH()
-{
-    cfg.config.pH = pH.read_ph();
+void PHSENSOR::setPH() {
+  phContainer.ph = pH.read_ph();
+}
+/**
+ * @brief Get the PH object from the pH sensor
+ *  This function is used to get the pH value from the pH sensor.
+ * @return const PHSENSOR::PH_t*
+ */
+const PHSENSOR::PH_t* PHSENSOR::getPH() {
+  const PH_t* phContainer = &this->phContainer;
+  return phContainer;
 }
 
-void PHSENSOR::phSensorLoop()
-{
-    if (_input_string_complete == true)
-    {                                                     // check if data received
-        _inputstring.toCharArray(_inputstring_array, 30); // convert the string to a char array
-        parse_cmd(_inputstring_array);                    // send data to pars_cmd function
-        _input_string_complete = false;                   // reset the flag used to tell if we have received a completed string from the PC
-        _inputstring = "";                                // clear the string
-    }
-    getPH();
-    Serial.println(cfg.config.pH);
+void PHSENSOR::phSensorLoop() {
+  if (_input_string_complete == true) {  // check if data received
+    parse_cmd(_inputstring.c_str());     // send data to pars_cmd function
+    _input_string_complete = false;  // reset the flag used to tell if we have
+                                     // received a completed string from the PC
+    _inputstring = "";               // clear the string
+  }
+  getPH();
+  log_i("%.3f", pH.read_ph());
 }
 
-void PHSENSOR::eventListener(const char *topic, const uint8_t *payload, uint16_t length)
-{
-    // The message is for the pH sensor.
-    // The message is in the format:
-    // <pH>:<pH_value>
-    String result;
-    for (int i = 0; i < length; i++)
-    {
-        log_i("%s", (char)payload[i]);
-        result += (char)payload[i];
-    }
+void PHSENSOR::eventListener(const char* topic,
+                             const uint8_t* payload,
+                             uint16_t length) {
+  // The message is for the pH sensor.
+  // The message is in the format:
+  // <pH>:<pH_value>
+  std::string result;
+  for (int i = 0; i < length; i++) {
+    log_i("%s", (char)payload[i]);
+    result += (char)payload[i];
+  }
 
-    // Act on the message
-    if (result.equalsIgnoreCase("UP MED"))
-    {
-        log_i("Adjust pH UP a medium amount!");
-        digitalWrite(_phUpPIN, HIGH);
-        my_delay(_doseTimeMed);
-        digitalWrite(_phUpPIN, LOW);
-    }
-    else if (result.equalsIgnoreCase("DOWN MED"))
-    {
-        log_i("Adjusting pH DOWN a medium amount!");
-        digitalWrite(_phDnPIN, HIGH);
-        my_delay(_doseTimeMed);
-        digitalWrite(_phDnPIN, LOW);
-    }
-    else if (result.equalsIgnoreCase("UP SM"))
-    {
-        log_i("Adjust pH UP a small amount!");
-        digitalWrite(_phUpPIN, HIGH);
-        my_delay(_doseTimeSm);
-        digitalWrite(_phUpPIN, LOW);
-    }
-    else if (result.equalsIgnoreCase("DOWN SM"))
-    {
-        log_i("Adjusting pH DOWN a small amount!");
-        digitalWrite(_phDnPIN, HIGH);
-        my_delay(_doseTimeSm);
-        digitalWrite(_phDnPIN, LOW);
-    }
-    else if (result.equalsIgnoreCase("UP LG"))
-    {
-        log_i("Adjust pH UP a large amount!");
-        digitalWrite(_phUpPIN, HIGH);
-        my_delay(_doseTimeLg);
-        digitalWrite(_phUpPIN, LOW);
-    }
-    else if (result.equalsIgnoreCase("DOWN LG"))
-    {
-        log_i("Adjusting pH DOWN a large amount!");
-        digitalWrite(_phDnPIN, HIGH);
-        my_delay(_doseTimeLg);
-        digitalWrite(_phDnPIN, LOW);
-    }
+  // Convert to lower case using lambda
+  std::for_each(result.begin(), result.end(),
+                [](char& c) { c = ::tolower(c); });
+
+  // Act on the message
+  if (result == "up med") {
+    log_i("Adjust pH UP a medium amount!");
+    digitalWrite(_phUpPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeMed);
+    digitalWrite(_phUpPIN, LOW);
+  } else if (result == "down med") {
+    log_i("Adjusting pH DOWN a medium amount!");
+    digitalWrite(_phDnPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeMed);
+    digitalWrite(_phDnPIN, LOW);
+  } else if (result == "up sm") {
+    log_i("Adjust pH UP a small amount!");
+    digitalWrite(_phUpPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeSm);
+    digitalWrite(_phUpPIN, LOW);
+  } else if (result == "down sm") {
+    log_i("Adjusting pH DOWN a small amount!");
+    digitalWrite(_phDnPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeSm);
+    digitalWrite(_phDnPIN, LOW);
+  } else if (result == "up lg") {
+    log_i("Adjust pH UP a large amount!");
+    digitalWrite(_phUpPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeLg);
+    digitalWrite(_phUpPIN, LOW);
+  } else if (result == "down lg") {
+    log_i("Adjusting pH DOWN a large amount!");
+    digitalWrite(_phDnPIN, HIGH);
+    Network_Utilities::my_delay(_doseTimeLg);
+    digitalWrite(_phDnPIN, LOW);
+  }
 }
 
 PHSENSOR phsensor;
--- Code/PlatformIO Project/lib/GreenHouseTowerDIY/src/local/io/sensors/pH/pHsensor.hpp
+++ Code/PlatformIO Project/lib/GreenHouseTowerDIY/src/local/io/sensors/pH/pHsensor.hpp
@@ -5,44 +5,56 @@
 #pragma once
 #ifndef PHSENSOR_HPP
 #define PHSENSOR_HPP
-#include <defines.hpp>
 #include <ph_grav.h>
+#include <string>
+#include <utilities/network_utilities.hpp>
 
-class PHSENSOR
-{
-public:
-    // Constructor
-    PHSENSOR();
-    virtual ~PHSENSOR();
+class PHSENSOR {
+ public:
+  // Constructor
+  PHSENSOR();
+  virtual ~PHSENSOR();
+  void begin();
+  void phSensorLoop();
+  void eventListener(const char* topic,
+                     const uint8_t* payload,
+                     uint16_t length);
+  void parse_cmd(const char* string);
 
-    void phSensorLoop();
-    void eventListener(const char *topic, const uint8_t *payload, uint16_t length);
-    void parse_cmd(char *string);
-    void getPH();
+  struct PH_t {
+    float ph;
+  };
 
-    // Friends
-    friend class BASEMQTT;
-    friend void onMqttMessage(const char *topic, const uint8_t *payload, uint16_t length);
-    friend void callback(char *topic, byte *payload, unsigned int length);
-    friend void onMqttConnected();
-    friend class HASSMQTT;
+  PH_t phContainer;
 
-private:
-    // Private functions
-    void serialEvent();
+  const PH_t* getPH();
+  void setPH();
 
-    // Private variables
-    const char *_pHTopic;
-    const char *_pHOutTopic;
-    int _phDnPIN;
-    int _phUpPIN;
-    int _doseTimeSm;
-    int _doseTimeMed;
-    int _doseTimeLg;
-    String _inputstring;            // a string to hold incoming data from the PC
-    boolean _input_string_complete; // a flag to indicate have we received all the data from the PC
-    char _inputstring_array[10];    // a char array needed for string parsing
+  // Friends
+  friend class BASEMQTT;
+  friend void onMqttMessage(const char* topic,
+                            const uint8_t* payload,
+                            uint16_t length);
+  friend void callback(char* topic, uint8_t* payload, unsigned int length);
+  friend void onMqttConnected();
+  friend class HASSMQTT;
+
+  Gravity_pH pH;
+
+ private:
+  // Private functions
+  void serialEvent();
+
+  // Private variables
+  const char* _pHTopic;
+  const char* _pHOutTopic;
+  int _phDnPIN;
+  int _phUpPIN;
+  int _doseTimeSm;
+  int _doseTimeMed;
+  int _doseTimeLg;
+  std::string _inputstring;        // a string to hold incoming data from the PC
+  boolean _input_string_complete;  // a flag to indicate have we received all
+                                   // the data from the PC
 };
-
-extern PHSENSOR phsensor;
-#endif // PHSENSOR_HPP
+#endif  // PHSENSOR_HPP
