556b13148539f271018cc8a0223dab23e9232e92
DaOfficialWizard <45744329+ZanzyTHEbar@users.noreply.github.com>
2023-06-11 01:07:31 +0100

    refactor: cleaning up project

--- Code/PlatformIO Project/tools/createwokwi.py
+++ Code/PlatformIO Project/tools/createwokwi.py
@@ -1,45 +1,41 @@
 #!/usr/bin/env python3
-
 # https://docs.wokwi.com/vscode/getting-started
 
-import toml
+# TODO: Add generation of diagram.json file per board environment
+
 Import("env")
+try:
 
+    def createTOML(source, target, env):
+        try:
+            firmware_name = env.subst("$BUILD_DIR\${PROGNAME}")
+            firmware_name = firmware_name.split(".pio")[1]
 
-def createTOML(source, target, env):
-    try:
-        firmware_name = env.subst("$BUILD_DIR\${PROGNAME}")
+            firmware_name = firmware_name.replace("\\", "/")
 
-        # find ".pio" in the firmware_name and split the string
-        # to get the project name
-        firmware_name = firmware_name.split(".pio")[1]
+            print("Creating wokwi.toml for %s" % firmware_name)
 
-        # replace the leading "\" with a "/"
+            wokwi_string = """\
+[wokwi]
+version = 1
+elf = ".pio{name}.elf"
+firmware = ".pio{name}.bin"
+[[net.forward]]
+from = "localhost:8180"
+to = "target:80"
+"""
+            toml_string = wokwi_string.format(name=firmware_name)
+            print(toml_string)
+            with open("wokwi.toml", "w") as f:
+                f.write(toml_string)
+                f.close()
 
-        firmware_name = firmware_name.replace("\\", "/")
+            print("wokwi.toml created \n")
 
-        print("Creating wokwi.toml for %s" % firmware_name)
+        except Exception as e:
+            print("Error creating wokwi.toml: %s" % e)
 
-        wokwi_string = """\
-        [wokwi]
-        version = 1
-        elf = ".pio{name}.elf"
-        firmware = ".pio{name}.bin"
-        """
-        toml_string = wokwi_string.format(name=firmware_name)
-        print(toml_string)
+    env.AddPostAction("$BUILD_DIR\${PROGNAME}.bin", createTOML)
 
-        parsed_toml = toml.loads(toml_string)
-
-        new_toml_string = toml.dumps(parsed_toml)
-        print(new_toml_string)
-
-        with open("wokwi.toml", "w") as f:
-            new_toml_string = toml.dump(parsed_toml, f)
-        print("wokwi.toml created \n")
-
-    except Exception as e:
-        print("Error creating wokwi.toml: %s" % e)
-
-
-env.AddPostAction("$BUILD_DIR\${PROGNAME}.bin", createTOML)
+except Exception as e:
+    print("Error creating wokwi.toml: %s" % e)
